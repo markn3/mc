647344544105ee8d88e6ace274c1458eee7ec0b0
markn3 <marknavalta.3@gmail.com>
2022-11-21 14:31:25 -0600

    Added some comments

--- cnn/predict.py
+++ cnn/predict.py
@@ -1,4 +1,5 @@
 import cv2
+
 class image_classifer:
 
     # constructor
@@ -18,15 +19,18 @@ class image_classifer:
         new_array = cv2.resize(image, (360, 640))
         return new_array.reshape(1, 360, 640, 3)
 
-    
+
     def classify(self, prepped_img):
         classification = "NotCave"
         prediction = self.model.predict(prepped_img, verbose=0)
+        # 30% threshold (subject to change)
         if(prediction[[0][0]] < 0.30):
             classification = "Cave"
+        # calls cave count
         found  = self.cave_count(classification)
         return classification, prediction, found
     
+    # if agent is in "cave" for 10 predictions then cave is found
     def cave_count(self, classification):
         if(classification == "Cave"):
             self.count += 1
